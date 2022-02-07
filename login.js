68a03c7e5e2e24f25e2931fc0133ea197abcab5a
Ningsang Jabegu <ningsanglimbu5203@gmail.com>
2022-02-08 00:26:41 +0545

    Login form added

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-document.getElementById("order-btn")
+/*document.getElementById("order-btn")
 .addEventListener ("click", function() {
     document.getElementsByClassName("popup-login")
     [0].classList.add("active");
@@ -8,4 +8,14 @@ document.getElementById("dismiss-popup-button")
 .addEventListener ("click", function() {
     document.getElementsByClassName("popup-login")
     [0].classList.remove("active");
-});
+});*/
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
